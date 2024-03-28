d001f690d576701d17fbaed59eebe952521233a9
ecpieritz <ecpieritz@gmail.com>
2024-03-28 16:41:02 -0300

    feat: add base pages

---
+++ src/pages/prices.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+
+export default function Prices() {
+  return (
+    <div>
+      <Header />
+      <h1>Prices</h1>
+    </div>
+  );
+}
---
+++ src/pages/services.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+
+export default function Services() {
+  return (
+    <div>
+      <Header />
+      <h1>Services</h1>
+    </div>
+  );
+}
