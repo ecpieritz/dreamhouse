bdfd42a3d607226dac64aeb36e843f82c81a34b8
ecpieritz <ecpieritz@gmail.com>
2024-04-02 11:27:10 -0300

    fix: add bootstrap

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
-import Head from 'next/head';
-import Header from '../../components/Header';
-import HomeBanner from '../../components/home/Banner';
+import Head from "next/head";
+import Header from "../../components/Header";
+import HomeBanner from "../../components/home/Banner";
+import HomeServices from "../../components/home/Services";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.png" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous"
+        ></link>
+
         <title>DreamHouse</title>
       </Head>
 
@@ -16,6 +24,17 @@ export default function Home() {
       <main>
         <HomeBanner />
       </main>
+
+      <script
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"
+      ></script>
     </>
   );
 }
