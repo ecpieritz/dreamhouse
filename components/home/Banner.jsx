a1177342982237eff58bb4284f2208f8a1cbbc7f
ecpieritz <ecpieritz@gmail.com>
2024-04-02 11:09:09 -0300

    feat: add responsive on home banner

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
 export default function HomeBanner() {
+  const [bannerImage, setBannerImage] = useState('/imgs/dh-home--banner.png');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 680) {
+        setBannerImage('/imgs/dh-home--banner-mobile.png');
+      } else {
+        setBannerImage('/imgs/dh-home--banner.png');
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className={styles.homeBanner} style={{backgroundImage: "url('/imgs/dh-home--banner.png')"}}>
+    <div className={styles.homeBanner} style={{backgroundImage: `url(${bannerImage})`}}>
       <h1>Let's make your <br className="mob-none"/>home beautiful together</h1>
 
       <p>Discover how our interior design experts can transform your space into something truly extraordinary.</p>
