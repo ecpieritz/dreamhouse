9562a65f5aac3a3a2feef813a46ffc63553d7059
ecpieritz <ecpieritz@gmail.com>
2024-04-07 16:57:51 -0300

    feat: add Counter function

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -2,11 +2,22 @@ import { useState, useEffect } from "react";
 import styles from "../styles/Counters.module.css";
 
 export default function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count < props.number) {
+      const interval = setInterval(() => {
+        setCount(prevCount => prevCount + 1);
+      }, 10); // Ajuste o intervalo conforme necessário para a velocidade de contagem desejada
+      
+      return () => clearInterval(interval);
+    }
+  }, [count, props.number]);
 
   return (
     <div className="col-md-3 col-sm-6">
       <div className={styles.dhCounter}>
-        <h2>{props.number}</h2>
+        <h2>{count}</h2>
         <p>{props.text}</p>
       </div>
     </div>
