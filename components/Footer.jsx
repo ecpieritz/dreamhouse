61c22b540fbd7ce0bfba0a77211a1b310b80df88
ecpieritz <ecpieritz@gmail.com>
2024-04-09 11:18:06 -0300

    feat: add footer + css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Footer.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Footer() {
@@ -8,45 +8,92 @@ export default function Footer() {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <Link className={styles.dhLogo} href='/'>
-              <Image src="/imgs/dh-logo.png" width={225} height={50} alt="Logo" />
+            <Link className={styles.dhLogo} href="/">
+              <Image
+                src="/imgs/dh-logo.png"
+                width={225}
+                height={50}
+                alt="Logo"
+              />
             </Link>
-            <p>Discover how our interior design experts can transform your space into something truly extraordinary.</p>
+            <p>
+              Discover how our interior design experts can transform your space
+              into something truly extraordinary.
+            </p>
 
             <div className={styles.dhSocials}>
               <Link href="/">
-                <Image src="/imgs/icons/dh-icons--facebook.png" width={30} height={30} alt="facebook" />
+                <Image
+                  src="/imgs/icons/dh-icons--facebook.png"
+                  width={30}
+                  height={30}
+                  alt="facebook"
+                />
               </Link>
               <Link href="/">
-                <Image src="/imgs/icons/dh-icons--linkedin.png" width={30} height={30} alt="linkedin" />
+                <Image
+                  src="/imgs/icons/dh-icons--linkedin.png"
+                  width={30}
+                  height={30}
+                  alt="linkedin"
+                />
               </Link>
               <Link href="/">
-                <Image src="/imgs/icons/dh-icons--instagram.png" width={30} height={30} alt="instagram" />
+                <Image
+                  src="/imgs/icons/dh-icons--instagram.png"
+                  width={30}
+                  height={30}
+                  alt="instagram"
+                />
               </Link>
             </div>
           </div>
           <div className="col-md-2">
-            <h4>Pages</h4>
-            <Link className={styles.dhLinks} href='/about'>About</Link>
-            <Link className={styles.dhLinks} href='/services'>Services</Link>
-            <Link className={styles.dhLinks} href='/prices'>Prices</Link>
-            <Link className={styles.dhLinks} href='/projects'>Projects</Link>
+            <h4 className={styles.dhTitle}>Pages</h4>
+
+            <div className={styles.dhPages}>
+              <Link className={styles.dhLinks} href="/about">
+                About
+              </Link>
+              <Link className={styles.dhLinks} href="/services">
+                Services
+              </Link>
+              <Link className={styles.dhLinks} href="/prices">
+                Prices
+              </Link>
+              <Link className={styles.dhLinks} href="/projects">
+                Projects
+              </Link>
+            </div>
           </div>
           <div className="col-md-3">
-            <h4>Services</h4>
-            <p>Design Consultation</p>
-            <p>Layout Design</p>
-            <p>Project Management</p>
-            <p>Renovation and Remodeling</p>
-            <p>Space Planning</p>
-            <p>Sustainable Solutions</p>
+            <h4 className={styles.dhTitle}>Services</h4>
+
+            <div className={styles.dhServices}>
+              <p>Design Consultation</p>
+              <p>Layout Design</p>
+              <p>Project Management</p>
+              <p>Renovation and Remodeling</p>
+              <p>Space Planning</p>
+              <p>Sustainable Solutions</p>
+            </div>
           </div>
           <div className="col-md-3">
-            <h4>Contact</h4>
-            <p>5th street, Maguari District</p>
-            <p>Ananindeua/PA, Brazil</p>
-            <Link className={styles.dhLinks} href="mailto:contact@dreamhouse.design">contact@dreamhouse.design</Link>
-            <Link className={styles.dhLinks} href="tel:+5591987654321">+55 91 98765-4321</Link>
+            <h4 className={styles.dhTitle}>Contact</h4>
+
+            <div className={styles.dhContact}>
+              <p>5th street, Maguari District</p>
+              <p>Ananindeua/PA, Brazil</p>
+              <Link
+                className={styles.dhLinks}
+                href="mailto:contact@dreamhouse.design"
+              >
+                contact@dreamhouse.design
+              </Link>
+              <Link className={styles.dhLinks} href="tel:+5591987654321">
+                +55 91 98765-4321
+              </Link>
+            </div>
           </div>
         </div>
       </div>
